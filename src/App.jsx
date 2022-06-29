632e2ee5e7ddde51ad6fa7c39e91ca5579dc9d0e
Darnell Champen <zerosalvation@gmail.com>
2022-06-29 14:17:23 -0400

    - created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/header';
 
 const App = () => {
   return (
     <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h3>Yippy - The Premier Giphy Search Engine</h3>
+        </div>
+    )
+}
+
+export default Header
